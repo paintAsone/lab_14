bf35b19521f89ce4e580ee9de859cc184844cf6f
NATKAMONLAK <paintlpch@gmail.com>
2021-02-02 15:35:02 +0700

    suffle

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -3,17 +3,36 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &,int &,int &);
+void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
+	int *w = &a, *x = &b, *y = &c, *z = &d;
 	
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
+}
+
+void shuffle(int *w,int *x,int *y,int *z){
+	int money[4] = {50,100,500,1000};
+	int a1,b1,c1,d1;
+
+	
+	do{
+		a1 = money[rand()%4];
+		b1 = money[rand()%4];
+		c1 = money[rand()%4];
+		d1 = money[rand()%4];
+	}while(a1 == b1 || a1 == c1 || a1 == d1 || b1 == c1 || b1 == d1 || c1 == d1);
+	
+	*w = a1;
+	*x = b1;
+	*y = c1;
+	*z = d1;
 }
